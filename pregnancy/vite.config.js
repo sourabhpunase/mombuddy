6ac4576667a17109db9afc8452cc4ee38222cb97
sourabhpunase <sourabhpunase19@gmail.com>
2025-07-31 03:03:15 +0530

    Simplify build config for stable deployment

--- pregnancy/vite.config.js
+++ pregnancy/vite.config.js
@@ -1,11 +1,21 @@
-export default {
-  plugins: [],
-  server: {
-    port: 5173,
-    open: true
-  },
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: true
   }
-}
+})
